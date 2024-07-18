01ae5065d7660d43b246525161770b42bf479679
Sameera Madushan Silva <madushansameera499@gmail.com>
2024-07-18 21:05:25 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 function setName(){
     var name=document.getElementById('name').value;
     if(name==""){
-        document.getElementById('error').style="display:block"
+        alert("Please Enter Your Name");
     }else{
-        document.cookie="username="+name;
-        //console.log(document.cookie.split('=')[1]);
-        window.location.href = window.location.href.replace("/index.html","")+"/html/questions.html";
-    }
-}
-
-function aboutMe(){
-    window.location.href = "https://www.linkedin.com/in/dev-sachira-madhushan/";
+         }
 }
